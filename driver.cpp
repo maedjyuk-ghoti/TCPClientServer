f840c72db19643de4e52aa13aff6c9d7707e6544
Ben Green <s.edwin.green@gmail.com>
2017-03-15 21:48:49 -0500

    Modified driver in preparation of ssl client and server

--- driver.cpp
+++ driver.cpp
@@ -2,24 +2,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "ssl_server.h"
+#include "ssl_client.h"
 #include "server.h"
 #include "client.h"
 
 int server()
 {
-	Server m_server;
+	Server* m_server = new Server();
 
-	if (m_server.Connect(5001))
+	if (m_server->Connect(5001))
 	{
 		std::cout << "Server connected\n";
 	}
 
 	while (true)
 	{
-		if (m_server.Listen())
+		if (m_server->Listen())
 		{
 			char buffer[100];
-			int bytes_received = m_server.Receive(buffer, sizeof(buffer));
+			int bytes_received = m_server->Receive(buffer, sizeof(buffer));
 			if (-1 == bytes_received)
 			{
 				perror("Error in Server::Receive");
@@ -30,25 +32,37 @@ int server()
 				std::cout << "Received " << bytes_received << " bytes" << std::endl;
 				std::cout << "Message contents: " << buffer << std::endl;
 			}
-			m_server.CloseClientSocket();
+			m_server->CloseClientSocket();
 		}
 	}
 	return 0;
 }
 
-int client()
+/**
+ * Starts up the client
+ * ssl - true if ssl client is desired
+ * Returns 0
+ */
+int client(bool const ssl)
 {
-	Client m_client;
-	char hostname[] = "localhost";
+	Client* m_client;
+	if (ssl)
+	{
+		m_client = new SSLClient();
+	}
+	else
+	{
+		m_client = new Client();
+	}
 
-	if (m_client.Connect(hostname, 5001))
+	char hostname[] = "localhost";
+	if (m_client->Connect(hostname, 5001))
 	{
 		std::cout << "Client connected\n";
 	}
 
 	char message[26] = "Hello from the other side";
-	ssize_t bytes_sent = m_client.Send(message, strlen(message));
-
+	ssize_t bytes_sent = m_client->Send(message, strlen(message));
 	if (strlen(message) == bytes_sent)
 	{
 		std::cout << "Successfully sent " << bytes_sent << " bytes" << std::endl;
@@ -57,6 +71,8 @@ int client()
 	{
 		std::cerr << "Error sending message" << std::endl;
 	}
+
+	delete m_client();
 
 	return 0;
 }
@@ -72,8 +88,12 @@ int main(int argc, char** argv)
 	{
 		return server();
 	}
+	else if (1 == atoi(argv[2]))
+	{
+		return client(false);
+	}
 	else
 	{
-		return client();
+		return client(true);
 	}
 }
