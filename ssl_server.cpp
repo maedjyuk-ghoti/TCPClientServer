8c548909f0ffec89b7c8a320c7b558e81d03da37
Ben Green <s.edwin.green@gmail.com>
2017-03-17 21:54:39 -0500

    filled out functions for ssl use

--- ssl_server.cpp
+++ ssl_server.cpp
@@ -1,36 +1,54 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <iostream>
+
 #include "ssl_server.h"
 
 SSLServer::SSLServer()
 	: Server()
 	, ssl_active(false)
 {
-	SSL_load_error_strings();
-	OpenSSL_add_ssl_algorithms();
+	InitOpenSSL();
+	GenerateContext();	
 }
 
 
 SSLServer::SSLServer(Server const &server)
-	: Server(Server)
+	: Server(server)
 	, ssl_active(false)
 {
+	InitOpenSSL();
+	GenerateContext();	
 }
 
 
 SSLServer::~SSLServer()
 {
+	Server::~Server();
+
+	SSL_free(ssl);
+	SSL_CTX_free(ssl_context);
+	EVP_cleanup();
 }
 
 
 bool SSLServer::Listen()
 {
-	if (Server::Listen())
+	if (0 != m_client_socket
+		|| Server::Listen())
 	{
-		SSL_CTX* ssl_context = CreateContext();
-		ConfigureContext(ssl_context);
-	}
+		ssl = SSL_new(ssl_context);
+		SSL_set_fd(ssl, m_client_socket);
 
+		if (SSL_accept(ssl) <= 0)
+		{
+			ERR_print_errors_fp(stderr);
+		}
+		else
+		{
+			return true;
+		}
+	}
 
 	return false;
 }
@@ -58,4 +76,37 @@ SSL_CTX* SSLServer::CreateContext()
 	}
 	
 	return ssl_context;
+}
+
+
+void SSLServer::ConfigureContext(SSL_CTX* const ssl_context)
+{
+	SSL_CTX_set_ecdh_auto(ssl_context, 1);
+
+	// Set the key and cert
+	if (SSL_CTX_use_certificate_file(ssl_context, "cert.pem", SSL_FILETYPE_PEM) < 0)
+	{
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	if (SSL_CTX_use_PrivateKey_file(ssl_context, "key,pem", SSL_FILETYPE_PEM) < 0)
+	{
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+void SSLServer::InitOpenSSL()
+{
+	SSL_load_error_strings();
+	OpenSSL_add_ssl_algorithms();
+}
+
+
+void SSLServer::GenerateContext()
+{
+	ssl_context = CreateContext();
+	ConfigureContext(ssl_context);
 }
